88fd1e95823a1854270c42e4f78a5044db8a5ee0
runeenglund <rune_ye@hotmail.com>
2022-05-24 08:28:48 +0200

    footer fix + burgermenu added

--- eksamensprojekt/script/burgermenu.js
+++ eksamensprojekt/script/burgermenu.js
@@ -1,16 +1,17 @@
 let menuToggler = document.querySelector('.menu-toggle');
 let nav = document.querySelector('.nav');
 let navLinks = Array.from(document.querySelectorAll('.nav-link'));
-let logo = document.querySelector('.logo');
+let contact = document.querySelector('.contact');
 
 menuToggler.addEventListener('click', ()=>{
   menuToggler.classList.toggle('active');
-  logo.classList.toggle('active');
   nav.classList.toggle('active');
+
+  contact.classList.remove('active');
 
   if (menuToggler.classList.contains('active')) {
     setTimeout(()=>{
-      navRight.classList.add('active')
+      nav.classList.add('active')
     },100)
 
     for (let i = 0; i < navLinks.length; i++) {
@@ -19,5 +20,8 @@ menuToggler.addEventListener('click', ()=>{
         navLinks[i].classList.add('active')
       }, i * 100)
     }
+    setTimeout(()=>{
+      contact.classList.add('active')
+    }, 800);
   }
 })
