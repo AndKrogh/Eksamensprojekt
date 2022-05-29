b048441d1cccd87b0a98f487e131632056505187
FreddieEgedal <95848391+FreddieEgedal@users.noreply.github.com>
2022-05-29 21:10:25 +0200

    ændringer

--- eksamensprojekt/script/frederik.js
+++ eksamensprojekt/script/frederik.js
@@ -8,26 +8,21 @@ function afdelingFunction() {
 
   for (let i = 0; i < afdelingArray.length; ++i) {
     dropdown[dropdown.length] = new Option(afdelingArray[i], afdelingArray[i]);
-    // linje 10 tager fat i linje 7 og oprætter en option hvor den tager fat i første element i array og sætter value og text til at være netop denne//
   }
 }
   afdelingFunction()
 //afdeling dropdown slut
 
 //God dag text start
-function hilsenfunction() {
-
-  const hour = new Date().getHours();
+  let time = new Date().getHours();
   let hilsen;
 
-  if (hour < 18) {
-    hilsen = "Tak for at henvende dig til os, hav en god dag!";
+  if (time < 18) {
+    hilsen = "Tak for at henvende dig til os, hav en fantastisk dag!";
   } else {
-    hilsen = "Tak for at henvende dig til os, hav en god aften";
+    hilsen = "Tak for at henvende dig til os, hav en fantastisk aften";
   }
 
   document.getElementById("hilsen").innerHTML = hilsen;
 }
-
-hilsenfunction()
 //God dag text slut
